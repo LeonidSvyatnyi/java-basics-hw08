8d567ff4cf9816b73a576728b7eab4b359794821
leonid.svyatnyi <svyatnyi.leonid@gmail.com>
2022-05-29 15:17:17 +0300

    Task Done

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
@@ -10,7 +13,14 @@ public class RegExp {
      * @return String with IP address
      */
     public static String getIpAddress(String text) {
-        // TODO fill in code here for extracting IP address using Regular Expression
-        return "";
+        String regEx =  "((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])";
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(text);
+        String newText = "";
+        while (matcher.find()) {
+            newText = matcher.group();
+        }
+
+        return newText;
     }
 }
